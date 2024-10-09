8ec73fa1b962ec836b2bc0e22bbcd6717ea0e635
PirarucuSebosoEsbelto <133917215+PirarucuSebosoEsbelto@users.noreply.github.com>
2024-10-09 08:28:09 -0300

    z

---
+++ perguntas.js
@@ -0,0 +1,102 @@
+criaCartao(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartao(
+    'Astronomia',
+    'Quais são os planetas do sistema solar em ordem de proximidade ao sol?',
+    'Mercúrio, Vênus, Terra, Marte, Júpiter, Saturno, Urano e Netuno.'
+)
+criaCartao(
+    'Biologia',
+    'Cite as partes da célula e suas funções principais.',
+    'Membrana plasmática, Parede celular, Pilus e Ribossomo.'
+)
+criaCartao(
+    'Química',
+    'Qual é a fórmula da água?',
+    'H2O.'
+)
+criaCartao(
+    'História',
+    'Quais foram as causas da Primeira Guerra Mundial?',
+    'Rivalidade econômica, ressentimentos por acontecimentos passados e questões nacionalistas.'
+)
+criaCartao(
+    'Física',
+    'Quais são os três estados da matéria?',
+    'Sólido, líquido e gasoso.'
+)
+criaCartao(
+    'História',
+    'O que é a Lei da Oferta e da Procura?',
+    'É um fenômeno que determina os preços dos produtos no mercado.'
+)
+criaCartao(
+    'Literatura',
+    'Quem foi o autor de "Dom Casmurro"?',
+    'Machado de Assis.'
+)
+criaCartao(
+    'Química',
+    'Quantos elementos existem na tabela periódica?',
+    '118 elementos.'
+)
+criaCartao(
+    'Química',
+    'O que caracteriza uma reação endotérmica?',
+    'Absorção de energia.'
+)
+criaCartao(
+    'Geografia',
+    'Quais são os principais tipos de solo do Brasil?',
+    'Massapê, Aluviais, Salmourão e Terra Roxa.'
+)
+criaCartao(
+    'Filosofia',
+    'Quem foram os principais filósofos da Grécia Antiga?',
+    'Sócrates, Platão e Aristóteles.'
+)
+criaCartao(
+    'Física',
+    'Quais são as leis de Newton?',
+    'Princípio da Inércia ou Primeira Lei de Newton; A Força ou Segunda Lei de Newton; Princípio da Ação e Reação ou Terceira Lei de Newton.'
+)
+criaCartao(
+    'História',
+    'O que é a Revolução Industrial?',
+    'É a transição para novos processos de produção.'
+)
+criaCartao(
+    'Biologia',
+    'Quais são os principais órgãos do sistema digestório humano?',
+    'Boca, faringe, esôfago, estômago, intestino delgado, intestino grosso, reto e ânus.'
+)
+criaCartao(
+    'Biologia',
+    'Qual é a definição de ecossistema?',
+    'É o termo utilizado para definir um grupo de seres que habitam em um determinado local, as relações entre eles, e a interação destas comunidades com o ambiente em que vivem.'
+)
+criaCartao(
+    'Biologia',
+    'Qual é a principal função da mitocôndria?',
+    'Produção de energia (ATP).'
+)
+criaCartao(
+    'Arte',
+    'Quais são os estilos artísticos do Renascimento?',
+    'Pintura, escultura, arquitetura e literatura.'
+)
+criaCartao(
+    'Biologia',
+    'O que define uma pirâmide alimentar?',
+    'É uma representação gráfica que ilustra a hierarquia dos diferentes grupos de alimentos e a quantidade recomendada de cada grupo para uma dieta equilibrada.'
+)
+criaCartao(
+    'Literatura',
+    'Quais são as principais características do romantismo na literatura?',
+    'O sentimentalismo, a supervalorização das emoções pessoais, o subjetivismo e egocentrismo.'
+)
+//Ao todo são 20 perguntas que exijam memorização
+// do seu ensino médio (qualquer disciplina)
