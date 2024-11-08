cde8b2980f67627d687601d1e9999ceaa109cd66
PirarucuSebosoEsbelto <133917215+PirarucuSebosoEsbelto@users.noreply.github.com>
2024-11-08 08:34:32 -0300

    fd

--- pagina2.js
+++ pagina2.js
@@ -18,4 +18,11 @@ container.appendChild(cartao);
 
 function mostrarResposta(card){
     const resposta = card.querySelector('.resposta-cartao');
+
+    if(resposta.style.display === "none"){
+        resposta.style.display = "block";
+    }
+    else{
+        resposta.style.display = "none";
+    }
 }
